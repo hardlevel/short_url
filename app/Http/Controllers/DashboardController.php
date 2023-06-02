4c646df00ce74b9d3f452bdaec50fbfc6e93a178
Rafael Tuneca <rtuneca@grupodisal.com.br>
2023-06-02 15:25:14 -0300

    created a controller for dashboard

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+use AshAllenDesign\ShortURL\Models\ShortURL;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        $links = ShortURL::all();
+        return view('dashboard', compact('links'));
+    }
+}
