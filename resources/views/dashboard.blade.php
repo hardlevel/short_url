faebf2faa25834afe35779c573668a42f2e95104
Rafael Tuneca <rtuneca@grupodisal.com.br>
2023-06-02 13:24:49 -0300

    added a table to show generated links

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,15 +4,33 @@
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
-
+    @auth
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
                     <a href="{{ route('links.index') }}">Novo link</a>
                 </div>
             </div>
+            <div class="">
+                <table class="table-auto">
+                    <thead>
+                        <tr>
+                            <th>Link original</th>
+                            <th>Link gerado</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($links as $link)
+                        <tr>
+                            <td>{{ $link->original-link }}</td>
+                            <td>{{ $link->new-link }}</td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
+    @endauth
 </x-app-layout>
